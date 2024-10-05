aa593e4cd1ce631d9ecf05d3dba80f863b51e092
xiaochenshiwo <112239918+xiaochenshiwo@users.noreply.github.com>
2024-10-05 20:27:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '04b157c8c57d4165a367f671c68136bf',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '001',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
